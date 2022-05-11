445fd81f573b148c10fe317fb433f672baa5532a
cadullms <cadullms@users.noreply.github.com>
2022-05-11 15:57:36 +0200

    friendly app

--- app/hello-world.py
+++ app/hello-world.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route('/', methods = ['POST','GET'])
 def index():
   if request.method == 'POST':
-    who_are_you = request.form.get('who_are_you')
+    whom_to_greet = request.form.get('whom_to_greet')
   else:
-    who_are_you = None
-  return render_template('index.html',who_are_you=who_are_you)
+    whom_to_greet = None
+  return render_template('index.html',whom_to_greet=whom_to_greet)
 
 if __name__ == '__main__':
   app.run()
